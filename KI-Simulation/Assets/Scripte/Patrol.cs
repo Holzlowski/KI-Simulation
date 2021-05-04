084b805a64f38d25232eaf42fdb53f4e4ec99401
Chantal Reißner <reissner99@gmail.com>
2021-05-04 15:11:58 +0200

    changed patrol, added follow
    changed patrol from 2 scripts to 1
    added follow on enemy cube which follows the patroling prey sphere

--- KI-Simulation/Assets/Scripte/Patrol.cs
+++ KI-Simulation/Assets/Scripte/Patrol.cs
@@ -5,18 +5,29 @@ using UnityEngine.AI;
 
 public class Patrol : MonoBehaviour
 {
-    public GameObject characterDestination;
+    public Transform destination;
+    public float minX;
+    public float maxX;
+    public float minZ;
+    public float maxZ;
+    public float y;
+
     NavMeshAgent theAgent;
 
     // Start is called before the first frame update
     void Start()
     {
         theAgent = GetComponent<NavMeshAgent>();
+        destination.position = new Vector3(Random.Range(minX, maxX), y, Random.Range(minZ, maxZ));
     }
 
     // Update is called once per frame
     void Update()
     {
-        theAgent.SetDestination(characterDestination.transform.position);
+        theAgent.SetDestination(destination.position);
+        if(Vector3.Distance(destination.position, theAgent.transform.position) < 0.2f)
+        {
+            destination.position = new Vector3(Random.Range(minX, maxX), y, Random.Range(minZ, maxZ));
+        }
     }
 }
