0634d37a764456785a0429707f497810261b5737
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2021-05-20 17:07:59 +0200

    Finite State Machine with cop and robber
    Ein Dieb kann jetzt spucken

--- KI-Simulation/Assets/Chase.cs
+++ KI-Simulation/Assets/Chase.cs
@@ -2,41 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Patrolling : NPCBaseFSM
+public class Chase : NPCBaseFSM
 {
-    GameObject[] wayPoints;
-    int currentWP;
-
-    private void Awake()
-    {
-        wayPoints = GameObject.FindGameObjectsWithTag("waypoint");
-    }
-
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         base.OnStateEnter(animator, stateInfo, layerIndex);
-        currentWP = 0;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (wayPoints.Length == 0) return;
-        if(Vector3.Distance(wayPoints[currentWP].transform.position, NPC.transform.position) < accuracy)
-        {
-            currentWP++;
-            if(currentWP >= wayPoints.Length)
-            {
-                currentWP = 0;
-            }
-        }
-
         //rotate towards target
-        Vector3 lookAtGoal = new Vector3(wayPoints[currentWP].transform.position.x, NPC.transform.position.y, wayPoints[currentWP].transform.position.z);
-        var direction = lookAtGoal - NPC.transform.position;
+        var direction = opponent.transform.position - NPC.transform.position;
         NPC.transform.rotation = Quaternion.Slerp(NPC.transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
-        NPC.transform.Translate(0, 0, Time.deltaTime * speed);
+        NPC.transform.Translate(0, 0, Time.deltaTime);
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
@@ -45,4 +25,15 @@ public class Patrolling : NPCBaseFSM
         
     }
 
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
 }
