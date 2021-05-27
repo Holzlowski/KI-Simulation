bf2c9994cd14d595da5192258d829d453dc5a5c3
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2021-05-27 08:50:35 +0200

    Changed projectile

--- KI-Simulation/Assets/Scripte/Projectile.cs
+++ KI-Simulation/Assets/Scripte/Projectile.cs
@@ -6,8 +6,15 @@ public class Projectile : MonoBehaviour
 {
     float delay = 2f;
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+    /*
     private void Update()
     {
         Destroy(gameObject, delay);
     }
+    */
 }
