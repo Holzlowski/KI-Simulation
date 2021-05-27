a181747ee64184ad5618ac25008879f00d5e3550
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2021-05-27 17:49:02 +0200

    Hunter Attack (not working)

--- KI-Simulation/Assets/Scenes/Holzis Scene/Scripte/HunterDistance.cs
+++ KI-Simulation/Assets/Scenes/Holzis Scene/Scripte/HunterDistance.cs
@@ -1,23 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class HunterDistance : MonoBehaviour
 {
     Animator anim;
+    NavMeshAgent agent;
     public GameObject prey;
+    Vector3 direction;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = prey.transform.position - this.transform.position;
+        float distance = Vector3.Distance(transform.position, prey.transform.position);
+        anim.SetFloat("distance", distance);
+
+       /*
+        if(distance <= agent.stoppingDistance)
+        {
+            anim.SetBool("attackDistance", true);
+            Debug.Log("Ich greif an");
+        }
+        else
+        {
+            anim.SetBool("attackDistance", false);
+        }
+       */
+
+        direction = prey.transform.position - this.transform.position;
         Debug.DrawRay(this.transform.position, direction, Color.green);
-        anim.SetFloat("distance", Vector3.Distance(transform.position, prey.transform.position));
     }
 }
