d505898dade3f25ec00fe869b0a3ad0f368e8481
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2021-06-21 16:23:23 +0200

    Hunter BT

--- KI-Simulation/Assets/Scenes/Holzis Scene/Scripte/HunterBT.cs
+++ KI-Simulation/Assets/Scenes/Holzis Scene/Scripte/HunterBT.cs
@@ -9,11 +9,20 @@ using Panda;
 public class HunterBT : MonoBehaviour
 {
     NavMeshAgent agentBT;
-    GameObject target;
-    List<GameObject> preys;
+    public GameObject target;
+    public List<GameObject> preys;
+    Animator anim;
 
     public float maxWanderDistance;
     public float accurracy;
+    public float visibleRange = 20;
+    public float damage;
+    public float healWithBite;
+
+    [Range(10f, 90f)]
+    public float whenIAmHungry = 50f;
+
+    float distance;
 
     float hunger;
     bool isHungry = false;
@@ -22,16 +31,27 @@ public class HunterBT : MonoBehaviour
     private void Start()
     {
         agentBT = gameObject.GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        preys = new List<GameObject>();
+        foreach (GameObject p in GameObject.FindGameObjectsWithTag("Prey"))
+        {
+            preys.Add(p);
+        }
     }
 
     private void Update()
     {
         hunger = GetComponent<HungerAllg>().hunger;
-        //preys = WorldManager.preys;
+        distanceToTarget();
+
+        if(target == null)
+        {
+            anim.SetBool("inAttackRange", false);
+        }
     }
 
     [Task]
-   public void wanderBT()
+    public void wanderBT()
     {
         // Get Random Point inside Sphere which position is transform.position, radius is maxWanderDistance
         Vector3 randomPos = Random.insideUnitSphere * maxWanderDistance + transform.position;
@@ -41,42 +61,30 @@ public class HunterBT : MonoBehaviour
         NavMesh.SamplePosition(randomPos, out hit, maxWanderDistance, NavMesh.AllAreas);
 
         agentBT.SetDestination(hit.position);
-        if(Vector3.Distance(hit.position, transform.position) <= accurracy)
+        if (Vector3.Distance(hit.position, transform.position) <= accurracy)
         {
             Task.current.Succeed();
-        }  
+            Debug.Log("Fertig");
+        }
     }
 
     [Task]
-    public bool hungerCheck(float percent)
+    public bool hungerCheck()
     {
-        if (this.hunger < this.GetComponent<HungerAllg>().maxHunger * percent)
+        if (this.hunger < this.GetComponent<HungerAllg>().maxHunger * (whenIAmHungry / 100))
         {
             return true;
         }
         else
         {
-            return false;
+           return false;
         }
-        //
     }
 
     [Task]
-    public bool checkDistanceToTarget(float visibleRange)
+    public bool checkDistanceToTarget()
     {
-        float distance = Mathf.Infinity;
-        bool isInRange = false;
-
-        for (int i = 0; i < preys.Count; i++)
-        {
-            GameObject p = preys[i];
-
-            if (Vector3.Distance(transform.position, p.transform.position) < distance)
-            {
-                target = p;
-                distance = Vector3.Distance(transform.position, p.transform.position);
-            }
-        }
+        bool isInRange;
         if (distance <= visibleRange)
         {
             isInRange = true;
@@ -92,7 +100,63 @@ public class HunterBT : MonoBehaviour
     [Task]
     public void moveToTarget()
     {
-        agentBT.SetDestination(target.transform.position);
+        if(target != null)
+        {
+            agentBT.SetDestination(target.transform.position);
+
+            if (agentBT.remainingDistance <= agentBT.stoppingDistance && !agentBT.pathPending)
+            {
+                Task.current.Succeed();
+            }
+        }
+  
+    }
+
+    [Task]
+    public void Attack()
+    {
+     
+            if (Vector3.Distance(transform.position, target.transform.position) <= agentBT.stoppingDistance)
+            {
+                anim.SetBool("inAttackRange", true);
+            Task.current.Succeed();
+        }
+            else
+            {
+                anim.SetBool("inAttackRange", false);
+            Task.current.Fail();
+            } 
+    }
+
+    public void makeDamage()
+    {
+        target.GetComponent<HungerAllg>().getdamage(damage);
+        GetComponent<HungerAllg>().eating(healWithBite);
+    }
+
+    public void distanceToTarget()
+    {
+      distance = Mathf.Infinity;
+        for (int i = 0; i<preys.Count; i++)
+        {
+                GameObject p = preys[i];
+
+            if(p == null)
+            {
+                preys.Remove(p);
+            }
+
+            if (Vector3.Distance(transform.position, p.transform.position) < distance)
+            {
+               target = p;
+               distance = Vector3.Distance(transform.position, p.transform.position);
+            }
+        }
+        if (target != null)
+        {
+            Vector3 direction = target.transform.position - this.transform.position;
+            Debug.DrawRay(this.transform.position, direction, Color.green);
+        }
     }
 }
 
